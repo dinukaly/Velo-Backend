d60ef99fb60efb468746aa3f52506175b2ab3f9e
Dinuka <infodinukalk@gmail.com>
2026-03-02 23:07:51 +0530

    create RegisterRequestDTO to transfer data when new user signing up

---
+++ src/main/java/com/dinukaly/velo/dto/RegisterRequestDTO.java
@@ -0,0 +1,10 @@
+package com.dinukaly.velo.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequestDTO {
+    private String email;
+    private String username;
+    private String password;
+}
