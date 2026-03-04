41b102e6c05e498da64786c4855bde4a103185f8
Dinuka <infodinukalk@gmail.com>
2026-03-04 16:19:29 +0530

    register modelMapper bean

--- src/main/java/com/dinukaly/velo/config/ApplicationConfig.java
+++ src/main/java/com/dinukaly/velo/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.dinukaly.velo.config;
 
 import com.dinukaly.velo.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -30,6 +31,10 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
