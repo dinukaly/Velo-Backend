984287dfb66a15f5d45f17a0231c7ff53059bb87
Dinuka <infodinukalk@gmail.com>
2026-03-04 16:52:59 +0530

    ProjectService with initial methods

---
+++ src/main/java/com/dinukaly/velo/service/ProjectService.java
@@ -0,0 +1,13 @@
+package com.dinukaly.velo.service;
+
+import com.dinukaly.velo.dto.CreateProjectRequestDTO;
+import com.dinukaly.velo.dto.ProjectResponseDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProjectService {
+    ProjectResponseDTO createProject(CreateProjectRequestDTO createProjectRequestDTO,String email);
+    List<ProjectResponseDTO> listProjects(String email);
+    void deleteProject(UUID projectId);
+}
