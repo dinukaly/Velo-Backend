e9e383a8c6fe5e2da0e2c3276d6474afc1e1d66c
Dinuka <infodinukalk@gmail.com>
2026-03-04 16:20:37 +0530

    refactor: add actual origin url

--- src/main/java/com/dinukaly/velo/controller/AuthController.java
+++ src/main/java/com/dinukaly/velo/controller/AuthController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     private final AuthService authService;
 
