a0d2074e097ffc1f35985cdcee457e3fa1eacfae
Dinuka <infodinukalk@gmail.com>
2026-03-04 23:26:47 +0530

    refactor: add description and language fields

--- src/main/java/com/dinukaly/velo/dto/ProjectResponseDTO.java
+++ src/main/java/com/dinukaly/velo/dto/ProjectResponseDTO.java
@@ -1,13 +1,21 @@
 package com.dinukaly.velo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectResponseDTO {
     private UUID id;
     private String name;
+    private String description;
+    private String language;
+    private java.time.Instant createdAt;
+    private java.time.Instant updatedAt;
 }
