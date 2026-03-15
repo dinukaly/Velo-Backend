1647adec65993457e18382a4901766867488affd
Dinuka <infodinukalk@gmail.com>
2026-03-15 20:22:47 +0530

    refactor(repository): add missing @Repository annotation

--- src/main/java/com/dinukaly/velo/repo/FileNodeRepository.java
+++ src/main/java/com/dinukaly/velo/repo/FileNodeRepository.java
@@ -3,10 +3,11 @@ package com.dinukaly.velo.repo;
 import com.dinukaly.velo.entity.FileNode;
 import com.dinukaly.velo.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
-
+@Repository
 public interface FileNodeRepository extends JpaRepository<FileNode, UUID> {
     List<FileNode> findByProject(Project project);
 
