ac46c87bcaccd0e2d5fe8a2768f075b0c1b6560b
Dinuka <infodinukalk@gmail.com>
2026-03-15 16:09:56 +0530

    refactor(repository): add missing @Repository annotation

--- src/main/java/com/dinukaly/velo/repo/SandboxRepository.java
+++ src/main/java/com/dinukaly/velo/repo/SandboxRepository.java
@@ -4,10 +4,11 @@ import com.dinukaly.velo.entity.Project;
 import com.dinukaly.velo.entity.SandboxSession;
 import com.dinukaly.velo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
-
+@Repository
 public interface SandboxRepository extends JpaRepository<SandboxSession, UUID> {
     Optional<SandboxSession> findByUser(User user);
 
